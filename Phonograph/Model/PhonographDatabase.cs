addfab49db3fdd7ea1106a42a452baae3fea06a8
Charles Macanka <cmacanka@gmail.com>
2014-04-06 15:51:27 -0400

    Added simple database object to model.
    This class extends the SQLiteConnection class, and is used to interact with
    the database.  Its constructor checks if a file exists at the specified path
    and, if one does not, creates a database there.  We shall see if this is
    generic enough to be in the model and not in the platform-specific code.

---
+++ Phonograph/Model/PhonographDatabase.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace Phonograph.Model
+{
+    class PhonographDatabase : SQLiteConnection
+    {
+        public PhonographDatabase(string path)
+            : base(path)
+        {
+            if (!System.IO.File.Exists(path))
+            {
+                CreateTable<Album>();
+                CreateTable<Artist>();
+                CreateTable<Play>();
+                CreateTable<Source>();
+                CreateTable<Track>();
+            }
+        }
+    }
+}
