f1ab1669a52bbd4b34f73dbc3c4c6343c2b424a5
Charles Macanka <cmacanka@gmail.com>
2014-04-06 21:09:25 -0400

    Added a "view" of the plays table.
    This is not a true view, as it is not in the database itself.  But it is a set
    of columns that are used after joins on all the tables the plays table
    references.  This needs to be done because the SQLite library does not support
    joins via linq to SQL, so a raw query must be used.

---
+++ Phonograph/Model/PlaysView.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phonograph.Model
+{
+    public class PlaysView
+    {
+        public int Id { get; set; }
+        public string TrackTitle { get; set; }
+        public string AlbumTitle { get; set; }
+        public string SourceName { get; set; }
+        public DateTime Time { get; set; }
+    }
+}
