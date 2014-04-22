148848925f9aecd9221a5f2530b31e4d457a42c1
Charles Macanka <cmacanka@gmail.com>
2014-04-21 23:32:56 -0400

    Added base broadcast receiver class
    This abstract class handles determining if a track should be recorded, based
    upon the current state of the music player, in a general sense.  The broadcast
    receivers for each specific music player would translate the app-specific
    values into these generic values.

--- Phonograph.Droid/PhonographService.cs
+++ Phonograph.Droid/PhonographService.cs
@@ -47,5 +47,127 @@ namespace Phonograph.Droid
                 return _service;
             }
         }
+
+        public abstract class PhonographServiceBaseBroadcastReceiver : BroadcastReceiver
+        {
+            protected string _currentTrackTitle;
+            protected string _currentAlbumTitle;
+            protected string _currentArtistName;
+            protected long _currentPosition;
+            protected long _currentDuration;
+            protected bool _isPlaying;
+
+            protected long _cumulativePlayedTime;
+
+            protected System.Diagnostics.Stopwatch _currentElapsedTimer;
+            protected System.Diagnostics.Stopwatch _sinceLastUpdateTimer;
+
+            public PhonographServiceBaseBroadcastReceiver()
+            {
+                _currentElapsedTimer = new System.Diagnostics.Stopwatch();
+                _sinceLastUpdateTimer = new System.Diagnostics.Stopwatch();
+            }
+
+            public void UpdateState(Context context, string currentTrackTitle, string currentAlbumTitle,
+                string currentArtistName, long currentPosition, long duration, bool isPlaying, long lastTrackPosition)
+            {
+                string message;
+
+                _sinceLastUpdateTimer.Stop();
+
+                // See how long it's been since the last time the state changed.
+                long timeSinceLastUpdate = _sinceLastUpdateTimer.ElapsedMilliseconds;
+                if (_currentElapsedTimer.IsRunning)
+                {
+                    _cumulativePlayedTime += timeSinceLastUpdate;
+                }
+
+                if (!(string.Equals(_currentTrackTitle, currentTrackTitle)
+                    && string.Equals(_currentAlbumTitle, currentAlbumTitle)
+                    && string.Equals(_currentArtistName, currentArtistName)))
+                {
+                    message = string.Format("New track: {0}, {1}, {2}, {3}/{4} ({5}%), {6}",
+                        currentArtistName, currentAlbumTitle, currentTrackTitle, currentPosition,
+                        duration, ((double)currentPosition) / duration, isPlaying);
+                    Android.Util.Log.Debug("PHONOGRAPH", message);
+                    Toast.MakeText(context, message, ToastLength.Short).Show();
+
+                    if (_currentDuration > 1
+                        && (double)_cumulativePlayedTime > (double)_currentDuration * 0.9)
+                    {
+                        message = "Would record last play here.";
+                        Android.Util.Log.Debug("PHONOGRAPH", message);
+                        Toast.MakeText(context, message, ToastLength.Short).Show();
+                    }
+                    else
+                    {
+                        message = string.Format("Did not play track long enough before changing. {0} <= {1}",
+                            (double)_cumulativePlayedTime, (double)_currentDuration * 0.9);
+                        Android.Util.Log.Debug("PHONOGRAPH", message);
+                        Toast.MakeText(context, message,
+                            ToastLength.Short).Show();
+                    }
+
+                    // Regardless of whether we recorded the play or not, we are in a new
+                    // song, so reset everything.
+                    _currentTrackTitle = currentTrackTitle;
+                    _currentAlbumTitle = currentAlbumTitle;
+                    _currentArtistName = currentArtistName;
+                    _currentDuration = duration;
+                    _cumulativePlayedTime = 0;
+
+                    // Start the timer for the [new] current song from 0.
+                    _currentElapsedTimer.Restart();
+                }
+                else
+                {
+                    message = string.Format("Same track: {0}, {1}, {2}, {3}/{4} ({5}%), {6}",
+                        currentArtistName, currentAlbumTitle, currentTrackTitle, currentPosition,
+                        duration, ((double)currentPosition) / duration, isPlaying);
+                    Android.Util.Log.Debug("PHONOGRAPH", message);
+                    Toast.MakeText(context, message, ToastLength.Short).Show();
+
+                    if (_currentDuration > 1
+                        && (double)_cumulativePlayedTime > (double)_currentDuration * 0.9)
+                    {
+                        message = string.Format("Would record current track here. {0} > {1}",
+                            _cumulativePlayedTime, (double)_currentDuration * 0.9);
+                        Android.Util.Log.Debug("PHONOGRAPH", message);
+                        Toast.MakeText(context, message, ToastLength.Short).Show();
+
+                        _cumulativePlayedTime = 0;
+                    }
+                    else
+                    {
+                        message = string.Format("Have not played current track long enough yet. {0} <= {1}",
+                            (double)_cumulativePlayedTime, (double)_currentDuration * 0.9);
+                        Android.Util.Log.Debug("PHONOGRAPH", message);
+                        Toast.MakeText(context, message,
+                            ToastLength.Short).Show();
+                    }
+                }
+
+                if (isPlaying && !_currentElapsedTimer.IsRunning)
+                {
+                    message = "Started current elapsed timer.";
+                    Android.Util.Log.Debug("PHONOGRAPH", message);
+                    Toast.MakeText(context, message, ToastLength.Short).Show();
+
+                    _currentElapsedTimer.Start();
+                }
+
+                if (!isPlaying && _currentElapsedTimer.IsRunning)
+                {
+                    message = "Stopped current elapsed timer.";
+                    Android.Util.Log.Debug("PHONOGRAPH", message);
+                    Toast.MakeText(context, message, ToastLength.Short).Show();
+
+                    _currentElapsedTimer.Stop();
+                }
+
+                _sinceLastUpdateTimer.Reset();
+                _sinceLastUpdateTimer.Start();
+            }
+        }
     }
 }
