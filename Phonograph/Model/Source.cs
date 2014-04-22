13427c63bb20277db255bd2fd4240383d5f9f776
Charles Macanka <cmacanka@gmail.com>
2014-04-22 01:21:05 -0400

    Fixed type of Source's Name property.

--- Phonograph/Model/Source.cs
+++ Phonograph/Model/Source.cs
@@ -14,7 +14,7 @@ namespace Phonograph.Model
         public int Id { get; set; }
 
         [Column("name")]
-        public int Name { get; set; }
+        public string Name { get; set; }
 
         [Column("description")]
         public string Description { get; set; }
