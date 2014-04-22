001d52035009012d94bbaf55ad1c3e4e88752d14
Charles Macanka <cmacanka@gmail.com>
2014-04-22 01:16:51 -0400

    Added artist name to plays view.

--- Phonograph/Model/PlaysView.cs
+++ Phonograph/Model/PlaysView.cs
@@ -11,6 +11,7 @@ namespace Phonograph.Model
         public int Id { get; set; }
         public string TrackTitle { get; set; }
         public string AlbumTitle { get; set; }
+        public string ArtistName { get; set; }
         public string SourceName { get; set; }
         public DateTime Time { get; set; }
     }
